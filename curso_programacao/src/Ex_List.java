986bbc06faebd10a6ae5df41b4c4a8d886bff3eb
gustavosouzaz <gustavodsouzaaa@gmail.com>
2025-01-16 22:45:21 -0300

    Praticando List

---
+++ curso_programacao/src/Ex_List.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ex_List {
+
+	public static void main(String[] args) {
+	
+		
+		List<String> list = new ArrayList<>();
+		
+		list.add("Gustavo");
+		list.add("Fernando");
+		list.add("Pedro");
+		list.add("Fabio");
+		list.add(2, "Enzo");
+		
+		System.out.println(list.size());
+		for(String z : list) {
+			System.out.println(z);
+		}
+		
+	}
+
+}
