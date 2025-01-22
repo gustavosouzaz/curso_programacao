5490f0433727c23b2d54deffa1c5cdcfcba6ca63
gustavosouzaz <gustavodsouzaaa@gmail.com>
2025-01-22 16:28:16 -0300

    obtendo uma unidade de tempo

--- curso_programacao/src/somando_date.java
+++ curso_programacao/src/somando_date.java
@@ -12,9 +12,10 @@ public class somando_date {
 		System.out.println(sdf.format(d));
 		Calendar cal = Calendar.getInstance();
 		cal.setTime(d);
-		cal.add(Calendar.HOUR_OF_DAY, 4);
-		d = cal.getTime();
-		System.out.println(sdf.format(d));
+		int minutes = cal.get(Calendar.MINUTE);
+		int month = 1 + cal.get(Calendar.MONTH);
+		System.out.println("Minutes: " + minutes);
+		System.out.println("Month: " + month);
 
 	}
 
