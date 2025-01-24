3fb49fb58f7db570115fca3ed00eecacacc361fc
gustavosouzaz <gustavodsouzaaa@gmail.com>
2025-01-24 17:00:21 -0300

    continuando

--- curso_programacao/src/Trabalhador_Exercicio.java
+++ curso_programacao/src/Trabalhador_Exercicio.java
@@ -1,3 +1,6 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -9,25 +12,34 @@ import entities.enums.WorkerLevel;
 
 public class Trabalhador_Exercicio {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 	
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
+		SimpleDateFormat sdf = new SimpleDateFormat("(DD/MM/YYYY)");
 		
-		System.out.println("Enter department's name: ");
+		System.out.print("Enter department's name: ");
 		String departmentName = sc.nextLine();
-		System.out.println("Enter wworker data: ");
-		System.out.println("Name: ");
+		System.out.print("Enter wworker data: ");
+		System.out.print("Name: ");
 		String workerName = sc.nextLine();
-		System.out.println("Level: ");
+		System.out.print("Level: ");
 		String workerLevel = sc.nextLine();
-		System.out.println("Base salary: ");
+		System.out.print("Base salary: ");
 		double baseSalary = sc.nextDouble();
 	    Worker worker = new Worker(workerName,WorkerLevel.valueOf(workerLevel), baseSalary);
 	    new Department(departmentName);
 	    
-	    
+	    System.out.print("How many contracts to this worker? ");
+		int n = sc.nextInt();
 		
+		
+		for (int i=1; i<=n; i++) {
+			 System.out.print("Enter contract #" + i + "data:");
+			 System.out.print("(DD/MM/YYYY)");
+			 Date contractDate = sdf.parse(sc.next());
+			 System.out.print("(DD/MM/YYYY)");
+		}
 		
       sc.close();
 	}
