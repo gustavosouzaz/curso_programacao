8fa36c6b447a2967525849d8af2c9c1a24db6670
gustavosouzaz <gustavodsouzaaa@gmail.com>
2025-01-24 13:34:49 -0300

    .

---
+++ curso_programacao/src/entidades/HourContract.java
@@ -0,0 +1,54 @@
+package entidades;
+
+import java.util.Date;
+
+public class HourContract {
+	
+	private Date date;
+	private Double valuePerHour;
+	private Integer hour;
+	
+	
+	public HourContract() {
+		super();
+	}
+
+
+	public HourContract(Date date, Double valuePerHour, Integer hour) {
+		super();
+		this.date = date;
+		this.valuePerHour = valuePerHour;
+		this.hour = hour;
+	}
+
+
+	public Date getDate() {
+		return date;
+	}
+
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+
+	public Double getValuePerHour() {
+		return valuePerHour;
+	}
+
+
+	public void setValuePerHour(Double valuePerHour) {
+		this.valuePerHour = valuePerHour;
+	}
+
+
+	public Integer getHour() {
+		return hour;
+	}
+
+
+	public void setHour(Integer hour) {
+		this.hour = hour;
+	}
+
+}
