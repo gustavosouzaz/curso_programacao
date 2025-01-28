417b6386b6f5eb60d49f9b5718fe1919f455bb94
gustavosouzaz <gustavodsouzaaa@gmail.com>
2025-01-28 14:56:04 -0300

    Exericio de Herança

---
+++ curso_programacao/src/entidades/OutsourcedEmployee.java
@@ -0,0 +1,27 @@
+package entidades;
+
+public class OutsourcedEmployee {
+
+	private Double additionalCharge;
+
+	public OutsourcedEmployee() {
+		super();
+	}
+
+	public OutsourcedEmployee(Double additionalCharge) {
+		super();
+		this.additionalCharge = additionalCharge;
+	}
+
+	public Double getAdditionalCharge() {
+		return additionalCharge;
+	}
+
+	public void setAdditionalCharge(Double additionalCharge) {
+		this.additionalCharge = additionalCharge;
+	}
+	
+	
+	
+	
+}
