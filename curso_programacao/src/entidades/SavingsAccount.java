416642c2f15aa6041a76d1622665c9b906b12264
gustavosouzaz <gustavodsouzaaa@gmail.com>
2025-01-28 14:05:52 -0300

    usando @Override e super em construutores

--- curso_programacao/src/entidades/SavingsAccount.java
+++ curso_programacao/src/entidades/SavingsAccount.java
@@ -28,5 +28,11 @@ public class SavingsAccount extends Account {
 	public void updateBalance() {
 		balance += balance * interestRate;
 	}
+	
+	@Override
+	public void withdraw(double amount) {
+		balance -= amount;
+	}
+
 
 }
