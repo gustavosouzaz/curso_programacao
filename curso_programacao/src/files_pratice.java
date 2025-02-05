7f1130d8a8aba2c895e054ab27e0dd2ed474c7a6
gustavosouzaz <gustavodsouzaaa@gmail.com>
2025-02-05 14:29:40 -0300

    Informações do caminho do     arquivo

--- curso_programacao/src/files_pratice.java
+++ curso_programacao/src/files_pratice.java
@@ -9,18 +9,9 @@ public class files_pratice {
 		System.out.println("Enter a folder path: ");
 		String strPath = sc.nextLine();
 		File path = new File(strPath);
-		File[] folders = path.listFiles(File::isDirectory);
-		System.out.println("FOLDERS:");
-		for (File folder : folders) {
-		System.out.println(folder);
-		}
-		File[] files = path.listFiles(File::isFile);
-		System.out.println("FILES:");
-		for (File file : files) {
-		System.out.println(file);
-		}
-		boolean success = new File(strPath + "\\subdir").mkdir();
-		System.out.println("Directory created successfully: " + success);
+		System.out.println("getPath: " + path.getPath());
+		System.out.println("getParent: " + path.getParent());
+		System.out.println("getName: " + path.getName());
 		sc.close();
 	}
 
