29723962a040040dbe4db3ee4eb22ca1361df368
gustavosouzaz <gustavodsouzaaa@gmail.com>
2025-02-05 13:28:54 -0300

    files2

--- curso_programacao/src/files_pratice.java
+++ curso_programacao/src/files_pratice.java
@@ -1,9 +1,24 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class files_pratice {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		
+		File file = new File("C:\\Work Eclipse\\in.txt");
+		Scanner sc = null;
+		
+		try {
+			sc = new Scanner(file);
+			while(sc.hasNextLine()) {
+				System.out.println(sc.nextLine());
+			}
+		}
+      catch (IOException e) {
+    	  System.out.println("Error: " + e.getMessage());
+      }
 	}
 
 }
