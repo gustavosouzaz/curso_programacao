52a54ea970bc88650d43b651277850e9586728b1
gustavosouzaz <gustavodsouzaaa@gmail.com>
2025-02-04 21:25:54 -0300

    toStirng

--- curso_programacao/src/entidades/Reservation.java
+++ curso_programacao/src/entidades/Reservation.java
@@ -59,5 +59,28 @@ public class Reservation {
 		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
 	}
 	
+	public void updateDates(Date checkIn, Date checkOut) {
+		Date now = new Date();
+		if (checkIn.before(now) || checkOut.before(now)) {
+			throw new DomainException("Reservation dates for update must be future dates");
+		}
+		if (!checkOut.after(checkIn)) {
+			throw new DomainException("Check-out date must be after check-in date");
+		}
+		this.checkin = checkin;
+		this.checkout = checkout;
+	}
+	@Override
+	public String toString() {
+		return "Room "
+			+ roomNumber
+			+ ", check-in: "
+			+ sdf.format(checkin)
+			+ ", check-out: "
+			+ sdf.format(checkout)
+			+ ", "
+			+ duration()
+			+ " nights";
+	}
 
 }
