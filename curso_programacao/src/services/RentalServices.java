520ca05e29f8861a8767c4d084f6009da3c51b02
gustavosouzaz <gustavodsouzaaa@gmail.com>
2025-02-06 00:41:13 -0300

    finalizando metodo

--- curso_programacao/src/services/RentalServices.java
+++ curso_programacao/src/services/RentalServices.java
@@ -1,7 +1,43 @@
 package services;
 
+import entidades.CarRental;
+import entidades.Invoice;
+
 public class RentalServices {
 	
+	private Double pricePerHour;
+	private Double pricePerDay;
 	
+	public RentalServices() {
+		super();
+	}
+
+	public RentalServices(Double pricePerHour, Double pricePerDay) {
+		super();
+		this.pricePerHour = pricePerHour;
+		this.pricePerDay = pricePerDay;
+	}
+
+	public Double getPricePerHour() {
+		return pricePerHour;
+	}
+
+	public void setPricePerHour(Double pricePerHour) {
+		this.pricePerHour = pricePerHour;
+	}
+
+	public Double getPricePerDay() {
+		return pricePerDay;
+	}
+
+	public void setPricePerDay(Double pricePerDay) {
+		this.pricePerDay = pricePerDay;
+	}
+	
+	public void processInvoice(CarRental carRental) {
+		
+		carRental.setInvoice(new Invoice(50.0, 10.0));
+		
+	}
 
 }
